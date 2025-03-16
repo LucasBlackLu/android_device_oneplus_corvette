c8417cfbcf1f9d85e15504bee96cf5fce99770a7
LucasBlackLu <lucasblack997@gmail.com>
2025-03-16 13:55:57 +0800

    corvette: Derpfest Bringup

--- derp_corvette.mk
+++ derp_corvette.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/corvette/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_corvette
+PRODUCT_NAME := derp_corvette
 PRODUCT_DEVICE := corvette
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
