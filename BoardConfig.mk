0aefb468a8c95481baad679ff92f52f6454ddd6b
sakurakyuo <sakurakyuo@protonmail.com>
2025-08-19 23:02:45 +0800

    corvette: set TARGET_SURFACEFLINGER_UDFPS_LIB to soong_config
    * vendor/lineage/config/BoardConfigSoong.mk:84: error: TARGET_SURFACEFLINGER_UDFPS_LIB is deprecated, please migrate to soong_config_set,surfaceflinger,udfps_lib

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@ TARGET_OTA_ASSERT_DEVICE := OP5D06L1
 TARGET_SCREEN_DENSITY := 480
 
 # Fingerprint
-TARGET_SURFACEFLINGER_UDFPS_LIB := //device/oneplus/corvette:libudfps_extension.corvette
+$(call soong_config_set,surfaceflinger,udfps_lib,//device/oneplus/corvette:libudfps_extension.corvette)
 
 # Kernel
 TARGET_KERNEL_ADDITIONAL_FLAGS += CONFIG_CORVETTE_DTB=y
